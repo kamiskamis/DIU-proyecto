97a266d811775dc27cfb2453ba373895d3eb2338
pinkyig <alejandrorojo483@gmail.com>
2024-11-01 22:20:23 -0300

    Refactor Navbar component to include logo and adjust user icon size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,41 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { CircleUserRound } from 'lucide-react';
-
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { CircleUserRound } from "lucide-react";
 const NavbarComponent = () => {
   return (
-    <Navbar style={{ backgroundColor: '#2E5E80' }} variant="dark" expand="lg">
+    <Navbar style={{ backgroundColor: "#2E5E80" }} variant="dark" expand="lg">
       <Container>
-      <Navbar.Brand as={Link} to="/">P.A.U</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-        <Nav.Link as={Link} to="/">Inicio</Nav.Link>
-        <Nav.Link as={Link} to="/about">Acerca</Nav.Link>
-        <Nav.Link as={Link} to="/contact">Contacto</Nav.Link>
-        </Nav>
+        <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
+          <img
+            alt=""
+            src="src/assets/logo2.png"
+            width="100"
+            height="60"
+            className="d-inline-block align-top me-2"
+          />{" "}
+          <span>P.A.U</span>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+              Inicio
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about">
+              Acerca
+            </Nav.Link>
+            <Nav.Link as={Link} to="/contact">
+              Contacto
+            </Nav.Link>
+          </Nav>
           {/* Apartado de Perfil */}
           <Nav className="ms-auto">
             <Nav.Link as={Link} to="/profile">
-              <CircleUserRound color="white" size={24} /> {/* Ícono de usuario */}
+              <CircleUserRound color="white" size={35} />{" "}
+              {/* Ícono de usuario */}
             </Nav.Link>
           </Nav>
-      </Navbar.Collapse>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
