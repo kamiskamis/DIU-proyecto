fe01fbbe15ddb1abf865a556deca6ecf06e534b6
Alejandro Rojo <alejandrorojo483@gmail.com>
2024-11-04 19:47:15 -0300

    Update Home component to use Link for Informatica dropdown item

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,30 @@
+import { Link } from 'react-router-dom';
 import Dropdown from 'react-bootstrap/Dropdown';
-import Button from 'react-bootstrap/Button';
 
 const Home = () => {
-    return (
-      <div className="container mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold mb-4">Página de Inicio</h1>
-        <p className="text-gray-600">Bienvenido a nuestra aplicación</p>
-        
-        <>
-      <Dropdown className="d-inline mx-2">
-        <Dropdown.Toggle id="dropdown-autoclose-true">
-          Departamento
-        </Dropdown.Toggle>
+  return (
+    <div className="container mx-auto px-4 py-8">
+    <h1 className="text-3xl font-bold mb-4">Página de Inicio</h1>
+    <p className="text-gray-600">Bienvenido a nuestra aplicación</p>
+    
+    <>
+    <Dropdown className="d-inline mx-2">
+    <Dropdown.Toggle id="dropdown-autoclose-true">
+      Departamento
+    </Dropdown.Toggle>
 
-        <Dropdown.Menu>
-          <Dropdown.Item href="#">Informatica</Dropdown.Item>
-          <Dropdown.Item href="#">Defider</Dropdown.Item>
-          <Dropdown.Item href="#">IDP</Dropdown.Item>
-          <Dropdown.Item href="#">Quimica</Dropdown.Item>
-        </Dropdown.Menu>
+    <Dropdown.Menu>
+      <Dropdown.Item as={Link} to="/informatica">Informatica</Dropdown.Item>
+      <Dropdown.Item href="#">Defider</Dropdown.Item>
+      <Dropdown.Item href="#">IDP</Dropdown.Item>
+      <Dropdown.Item href="#">Quimica</Dropdown.Item>
+    </Dropdown.Menu>
 
-        <Button variant="primary">Buscar</Button>{' '}
-      </Dropdown>
-    </>
-      </div>
-      
-    );
+    </Dropdown>
+  </>
+    </div>
+    
+  );
   };
   
   export default Home;
