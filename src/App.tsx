098048c61151bc8cd2103dc96cffa1e48d571601
Alejandro Rojo <alejandrorojo483@gmail.com>
2024-11-04 19:45:58 -0300

    Add route for Informatica page in App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Profile from './pages/Profile';
+import Informatica from './pages/Informatica';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/informatica" element={<Informatica />} />
       </Routes>
     </Router>
   );
