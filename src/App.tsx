6e41cc957826a2d62bd520d938615ea14f2f9b3e
Alejandro Rojo <alejandrorojo483@gmail.com>
2024-11-05 19:32:37 -0300

    Refactor App component to improve readability of Profile route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavbarComponent from './components/Navbar';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Profile from './pages/Profile';
-import Informatica from './pages/Informatica';
-import Resultados from './pages/Resultados';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavbarComponent from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Profile from "./pages/Profile";
+import Informatica from "./pages/Informatica";
+import Resultados from "./pages/Resultados";
 
 function App() {
   return (
@@ -16,7 +15,19 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/profile" element={<Profile name="Camila Rosales" rut="20562694-8" email="camila.rosales@usm.cl" rol="202173631-k" carreer="Ing. Civil Informatica" profilePicture="src/assets/IMG_7631.jpg" />} />
+        <Route
+          path="/profile"
+          element={
+            <Profile
+              name="Camila Rosales"
+              rut="20.562.694-8"
+              email="camila.rosales@usm.cl"
+              rol="202173631-k"
+              carreer="Ing. Civil Informatica"
+              profilePicture="src/assets/IMG_7631.jpg"
+            />
+          }
+        />
         <Route path="/informatica" element={<Informatica />} />
         <Route path="/resultados" element={<Resultados />} />
       </Routes>
