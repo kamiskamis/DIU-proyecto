8aac57f83dfa1aa220ef8e08c9ac0d9e120429f9
Alejandro Rojo <alejandrorojo483@gmail.com>
2024-11-04 21:41:16 -0300

    Add Resultados page component to display results

---
+++ src/pages/Resultados.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Resultados: React.FC = () => {
+    return (
+        <div className="container mx-auto px-4 py-8">
+            <h1>Resultados</h1>
+            <p>Aquí se mostrarán los resultados.</p>
+        </div>
+    );
+};
+
+export default Resultados;
