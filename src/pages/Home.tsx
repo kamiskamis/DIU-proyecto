4dd3fdcda65d60eba86d147ba64beae3fd01779a
Alejandro Rojo <alejandrorojo483@gmail.com>
2024-11-05 22:11:14 -0300

    Update Home component to enhance welcome message and provide guidance on department selection

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,13 @@ import Dropdown from "react-bootstrap/Dropdown";
 const Home = () => {
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-4">Página de Inicio</h1>
-      <p className="text-gray-600">Bienvenido a nuestra aplicación</p>
+      <h1 className="text-3xl font-bold mb-4">Bienvenido a PAU</h1>
+      <p className="text-gray-600 mt-4">
+        ¡Te damos la bienvenida a PAU, la Plataforma de Ayudantías Unificada!.
+        Aquí podrás seleccionar el departamento al cual deseas postular para una
+        ayudantía. Explora el menú desplegable para obtener más detalles sobre
+        las ayudantías disponibles en cada departamento.
+      </p>
 
       <>
         <Dropdown className="d-inline mx-2">
