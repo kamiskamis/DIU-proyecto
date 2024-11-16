5233796224e9b4b9fc828b3864c8fd550dcdbbe8
Alejandro Rojo <alejandrorojo483@gmail.com>
2024-11-15 21:11:02 -0300

    Refactor App component layout to include Footer and improve structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavbarComponent from "./components/Navbar";
+import Footer from "./components/Fotter"; // Asegúrate que el nombre del archivo es Footer.jsx
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -13,30 +14,35 @@ import Quimica from "./pages/Quimica";
 function App() {
   return (
     <Router>
-      <NavbarComponent />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route
-          path="/profile"
-          element={
-            <Profile
-              name="Camila Rosales"
-              rut="20.562.694-8"
-              email="camila.rosales@usm.cl"
-              rol="202173631-k"
-              carreer="Ing. Civil Informatica"
-              profilePicture="src/assets/IMG_7631.jpg"
+      <div className="flex flex-col min-h-screen">
+        <NavbarComponent />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route
+              path="/profile"
+              element={
+                <Profile
+                  name="Camila Rosales"
+                  rut="20.562.694-8"
+                  email="camila.rosales@usm.cl"
+                  rol="202173631-k"
+                  carreer="Ing. Civil Informatica"
+                  profilePicture="src/assets/IMG_7631.jpg"
+                />
+              }
             />
-          }
-        />
-        <Route path="/informatica" element={<Informatica />} />
-        <Route path="/resultados" element={<Resultados />} />
-        <Route path="/idp" element={<Idp />} />
-        <Route path="/matematicas" element={<Matematicas />} />
-        <Route path="/quimica" element={<Quimica />} />
-      </Routes>
+            <Route path="/informatica" element={<Informatica />} />
+            <Route path="/resultados" element={<Resultados />} />
+            <Route path="/idp" element={<Idp />} />
+            <Route path="/matematicas" element={<Matematicas />} />
+            <Route path="/quimica" element={<Quimica />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
 }
